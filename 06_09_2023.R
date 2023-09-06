17c128dc4340173bed89bea4652fde5c861d8dc3
jorowags <jwagge@gmail.com>
2023-09-06 17:41:14 -0500

    Update 06_09_2023.R
    I did things!

--- 06_09_2023.R
+++ 06_09_2023.R
@@ -2,7 +2,8 @@
 # The purpose of this script is to demonstrate how to find a mean of a data set.
 
 # if you don't have the "psych" package installed, remove the hashtag below and run the install command
-# install.packages("psych")
+install.packages("psych")
+install.packages("readr")
 
 library(psych)
 
@@ -12,3 +13,14 @@ installed_packages <- installed.packages()
 # Print the names of installed packages
 cat("Installed packages:\n")
 cat(installed_packages[, "Package"], sep = "\n")
+
+library(readr)
+avila_cameron_data <- read_csv("avila_cameron_data.csv")
+View(avila_cameron_data)
+
+# calculate total number of participants
+# Calculate the total number of rows
+total_rows <- nrow(avila_cameron_data)
+
+# Print the total number of rows
+cat("The total number of rows in the dataset is:", total_rows, "\n")
